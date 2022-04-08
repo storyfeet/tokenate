bb87d388b4d8fd00ecaf83dd4529c146fe797c48
Matthew Stoodley <matt.storyfeet@gmail.com>
2022-04-08 19:02:12 +0100

    progress on building return type

---
+++ src/charbool.rs
@@ -0,0 +1,30 @@
+pub trait CharBool {
+    fn cb(&self, _: char) -> bool;
+    fn expects(&self) -> String {
+        std::any::type_name(self);
+    }
+}
+
+impl<F: Fn(char) -> bool> CharBool for F {
+    fn cb(&self, c: char) -> bool {
+        self(c)
+    }
+}
+
+impl CharBool for &str {
+    fn cb(&self, c: char) -> bool {
+        self.contains(c)
+    }
+    fn expects(&self) -> String {
+        format("One of '{}'", self)
+    }
+}
+
+impl CharBool for char {
+    fn cb(&self, c: char) -> bool {
+        *self == c
+    }
+    expects(&self)->String{
+        format("Char '{}'",self)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+mod charbool;
+pub use charbool::CharBool;
 pub struct TErr {
-    line: u64,
-    cnum: u64,
-    index: u64,
+    line: usize,
+    cnum: usize,
+    index: usize,
     exp: &'static str,
     got: Option<char>,
 }
@@ -9,13 +11,16 @@ pub struct TErr {
 pub type TokenRes<'a, T> = Result<Option<Token<'a, T>>, TErr>;
 
 pub struct Token<'a, T> {
-    kind: T,
+    value: T,
     s: &'a str,
+    start: usize,
+    end: usize,
 }
 pub struct TokenChars<'a> {
     s: &'a str,
+    line: usize,
     chars: std::str::CharIndices<'a>,
-    start: u64,
+    start: usize,
     peek: Option<(usize, char)>,
 }
 
@@ -23,6 +28,7 @@ impl<'a> TokenChars<'a> {
     pub fn new(s: &'a str) -> Self {
         Self {
             s: s,
+            line: 1, //starts at 1 because that seems to be how text editors think
             chars: s.char_indices(),
             start: 0,
             peek: None,
@@ -31,7 +37,10 @@ impl<'a> TokenChars<'a> {
 
     pub fn next_char(&mut self) -> Option<(usize, char)> {
         match self.peek.take() {
-            Some((_,c))if c == '\n' 
+            Some((n, c)) if c == '\n' => {
+                self.line += 1;
+                Some((n, c))
+            }
             Some(c) => Some(c),
             None => self.chars.next(),
         }
@@ -58,14 +67,12 @@ impl<'a> TokenChars<'a> {
         }
     }
 
-    pub fn make_token_res<T>(&mut self, tt: T, unpeek: bool) -> TokenRes<'a, T> {
-        if unpeek {
-            self.peek = None
-        }
+    pub fn make_token_res<T>(&mut self, tt: T) -> TokenRes<'a, T> {
         Ok(Some(self.make_token(tt)))
     }
 
-    pub fn make_token<T>(&mut self, tt: T) -> Token<'a, T> {
+    pub fn make_token<T>(&mut self, value: T) -> Token<'a, T> {
+        self.unpeek();
         let start = self.start;
         let end = self.peek_index();
         self.start = end;
@@ -73,7 +80,7 @@ impl<'a> TokenChars<'a> {
             start,
             end,
             s: &self.s[start..end],
-            tt,
+            value,
         }
     }
 
@@ -88,49 +95,65 @@ impl<'a> TokenChars<'a> {
         }
     }
 
-    pub fn follow<T, F: Fn(char) -> T>(&mut self) -> TokenRes<'a, T> {}
 
-    pub fn next(&mut self) -> TokenRes<'a> {
-        let follow = |s: &mut Self, c: char, tt: TokenType<'a>| {
-            s.peek = None;
-            match s.next_char() {
-                Some((_, r)) if r == c => s.make_token_wrap(tt, false),
-                _ => e_str("Expected second Dot"),
-            }
-        };
-        let follow_def = |s: &mut Self, c: char, tt: TokenType<'a>, def: TokenType<'a>| {
-            s.peek = None;
-            match s.peek_char() {
-                Some((_, r)) if r == c => s.make_token_wrap(tt, true),
-                _ => s.make_token_wrap(def, false),
-            }
-        };
-        self.white_space();
-        self.start = self.peek_index();
-        let pc = match self.peek_char() {
-            None => return Ok(None),
-            Some(v) => v,
-        };
-        match pc.1 {
-            c if c >= '0' && c <= '9' => self.number(),
-            '\"' => self.qoth(),
-            '(' => self.make_token_wrap(TokenType::ParenO, true),
-            ')' => self.make_token_wrap(TokenType::ParenC, true),
-            '[' => self.make_token_wrap(TokenType::BraceO, true),
-            ']' => self.make_token_wrap(TokenType::BraceC, true),
-            '+' => follow_def(self, '+', TokenType::Append, TokenType::Add),
-            '-' => self.make_token_wrap(TokenType::Sub, true),
-            '$' => self.make_token_wrap(TokenType::Dollar, true),
-            ':' => self.make_token_wrap(TokenType::Colon, true),
-            ',' => self.make_token_wrap(TokenType::Comma, true),
-            '.' => follow(self, '.', TokenType::Range),
-            '=' => follow(self, '=', TokenType::Equal),
-            '<' => self.make_token_wrap(TokenType::Less, true),
-            '>' => self.make_token_wrap(TokenType::Greater, true),
-            '!' => self.make_token_wrap(TokenType::Count, true),
-            c if c.is_alphabetic() || c == '_' => self.unqoth(),
-
-            _ => e_str("Unexpected Character"),
+    fn expected<T>(&mut self, s: &str) -> TokenRes<'a, T> {
+        let (index,cnum )match self.peek_ne
+        TErr{
+            line: self.num,
+            index: 
+            exp: &'static str,
+            got: Option<char>,
         }
     }
+
+    pub fn require<T, C: CharBool>(&mut self, c: C, t: T) -> TokenRes<'a, T> {
+        self.unpeek(); //current peek
+        match self.next_char() {
+            Some((_, r)) if c.cb(r) => self.make_token_res(t),
+            _ => self.expected(c.expects()),
+        }
+    }
+
+    /*pub fn next(&mut self) -> TokenRes<'a> {
+    let follow = |s: &mut Self, c: char, tt: TokenType<'a>| {
+        s.peek = None;
+        match s.next_char() {
+            Some((_, r)) if r == c => s.make_token_wrap(tt, false),
+            _ => e_str("Expected second Dot"),
+        }
+    };
+    let follow_def = |s: &mut Self, c: char, tt: TokenType<'a>, def: TokenType<'a>| {
+        s.peek = None;
+        match s.peek_char() {
+            Some((_, r)) if r == c => s.make_token_wrap(tt, true),
+            _ => s.make_token_wrap(def, false),
+        }
+    };
+    self.white_space();
+    self.start = self.peek_index();
+    let pc = match self.peek_char() {
+        None => return Ok(None),
+        Some(v) => v,
+    };
+    match pc.1 {
+        c if c >= '0' && c <= '9' => self.number(),
+        '\"' => self.qoth(),
+        /*    '(' => self.make_token_wrap(TokenType::ParenO, true),
+        ')' => self.make_token_wrap(TokenType::ParenC, true),
+        '[' => self.make_token_wrap(TokenType::BraceO, true),
+        ']' => self.make_token_wrap(TokenType::BraceC, true),
+        '+' => follow_def(self, '+', TokenType::Append, TokenType::Add),
+        '-' => self.make_token_wrap(TokenType::Sub, true),
+        '$' => self.make_token_wrap(TokenType::Dollar, true),
+        ':' => self.make_token_wrap(TokenType::Colon, true),
+        ',' => self.make_token_wrap(TokenType::Comma, true),
+        '.' => follow(self, '.', TokenType::Range),
+        '=' => follow(self, '=', TokenType::Equal),
+        '<' => self.make_token_wrap(TokenType::Less, true),
+        '>' => self.make_token_wrap(TokenType::Greater, true),
+        '!' => self.make_token_wrap(TokenType::Count, true),
+        c if c.is_alphabetic() || c == '_' => self.unqoth(),*/
+            _ => self.expected("string"),
+        }
+    }*/
 }
